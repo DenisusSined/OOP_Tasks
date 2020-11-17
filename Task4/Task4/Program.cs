65653075b8023dd0bda3436e77378b11fc4b3353
Деніс Сідлецький <sidletskyi.d@donnu.edu.ua>
2020-11-17 11:07:58 +0200

    /

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.IO;
+namespace Task_4
+{
+    static class Line
+    {
+        private static int ax;
+        public static int Ax
+        {
+            get
+            {
+                return ax;
+            }
+            set
+            {
+                ax = value;
+            }
+        }
+        private static int ay;
+        public static int Ay
+        {
+            get
+            {
+                return ay;
+            }
+            set
+            {
+                ay = value;
+            }
+        }
+        private static int bx;
+        public static int Bx
+        {
+            get
+            {
+                return bx;
+            }
+            set
+            {
+                bx = value;
+            }
+        }
+        private static int by;
+        public static int By
+        {
+            get
+            {
+                return by;
+            }
+            set
+            {
+                by = value;
+            }
+        }
+        private static double s;
+        public static double S
+        {
+            get
+            {
+                return s;
+            }
+            set
+            {
+                s = value;
+            }
+        }
+
+        private static double llength;
+        public static double Llength
+        {
+            get
+            {
+                return llength;
+            }
+            set
+            {
+                llength = value;
+            }
+        }
+        public static string GetLength(int x1,int y1, int x2, int y2) // Довжина відрізка
+        {
+            llength = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
+            string result = String.Format("{0:f1}", llength);
+            return result;
+        }
+        public static double GetMid(int x, int y) // Середина відрізка
+        {
+            s = (x + y) / 2.0;
+            return s;
+        }
+        public static int[] GetScope(int ax, int ay, int bx, int by) // Масштабування відрізка
+        {
+            Console.WriteLine("У скільки разів потрібно масштабувати відрізок?");
+            int k = Convert.ToInt32(Console.ReadLine());
+            int bxs = bx + (bx - ax) * (k - 1);
+            int bys = by + (by - ay) * (k - 1);
+            int[] scopedb = {bxs, bys};
+            return scopedb;
+        }
+        class Program
+        {
+            static void Main()
+            {
+                Console.OutputEncoding = System.Text.Encoding.Unicode;
+                Console.InputEncoding = System.Text.Encoding.Unicode;
+                string xytxt;
+                try
+                {
+                    StreamReader sr = new StreamReader(@"D:\Унік\ООП\task4sr.txt");
+                    xytxt = sr.ReadLine();
+                    sr.Close();
+                    Console.WriteLine(xytxt);
+
+                }
+                catch (IOException exc)
+                {
+                    Console.WriteLine("Помилка доступу до файлу:" + exc.Message);
+                    return;
+                }
+                for (int i = 0; i < xytxt.Length; i++)
+                {
+                    ax = int.Parse(xytxt[0].ToString());
+                    ay = int.Parse(xytxt[1].ToString());
+                    bx = int.Parse(xytxt[2].ToString());
+                    by = int.Parse(xytxt[3].ToString());
+                }
+                Console.WriteLine($"A[{ax};{ay}],B[{bx};{by}]");
+                Console.WriteLine($"Довжина AB - {GetLength(ax, ay, bx, by)}");
+                Console.WriteLine($"Середина AB - [{GetMid(ax, bx)};{GetMid(ay, by)}]");
+                int[] scopedb = GetScope(ax, ay, bx, by);
+                Console.WriteLine($"Масштабований відрізок = A[{ax};{ay}], B[{scopedb[0]};{scopedb[1]}]");
+
+                try
+                {
+                    using StreamWriter sw = new StreamWriter(@"D:\Унік\ООП\task4sw.txt", false, System.Text.Encoding.Default);
+                    sw.WriteLine($"A[{ax};{ay}],B[{bx};{by}]");
+                    sw.WriteLine($"Довжина AB - {GetLength(ax, ay, bx, by)}");
+                    sw.WriteLine($"Середина AB - [{GetMid(ax, bx)};{GetMid(ay, by)}]");
+                    sw.WriteLine($"Масштабований відрізок = A[{ax};{ay}], B[{scopedb[0]};{scopedb[1]}]");
+                }
+                catch (IOException exc)
+                {
+                    Console.WriteLine("Помилка доступу до файлу:" + exc.Message);
+                    return;
+                }
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }       
+    }
+}
---
+++ Task5/Task5/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+using System.Reflection.PortableExecutable;
+
+namespace task5
+{
+    class Arrays
+    {
+        public int[] arr;
+        public Arrays(int[] a)  //конструктор
+        {
+            arr = a;
+        }
+        public int Check_xi(int xi)
+        {
+            if (xi % 2 == 0)
+            {
+                xi = xi - 8;
+            }
+            return xi;
+        }
+        public int Check_zi(int xi, int yi)
+        {
+            int zi = Convert.ToInt32(Math.Pow(Convert.ToDouble(yi), 2) - Math.Pow(Convert.ToDouble(xi), 2));
+            return zi;
+
+        }
+        public void Getarrays(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"[{array[i]}]");
+            }
+            Console.WriteLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string xt, yt;
+            try
+            {
+                StreamReader xsr = new StreamReader(@"D:\Унік\ООП\task5x.txt");
+                xt = xsr.ReadLine();
+                xsr.Close();
+                Console.WriteLine(xt);
+                StreamReader ysr = new StreamReader(@"D:\Унік\ООП\task5y.txt");
+                yt = ysr.ReadLine();
+                ysr.Close();
+                Console.WriteLine(yt);
+            }
+            catch (IOException exc)
+            {
+                Console.WriteLine("Помилка доступу до файлу:" + exc.Message);
+                return;
+            }
+
+            int[] array_x = new int[xt.Length];
+            int[] array_y = new int[yt.Length];
+            int[] array_z = new int[xt.Length];
+
+            for (int i = 0; i < xt.Length; i++)
+            {
+                array_x[i] = int.Parse(xt[i].ToString());
+            }
+
+            for (int i = 0; i < yt.Length; i++)
+            {
+                array_y[i] = int.Parse(yt[i].ToString());
+            }
+
+            Arrays x = new Arrays(array_x);
+            Arrays y = new Arrays(array_y);
+            Arrays z = new Arrays(array_z);
+
+            for (int i = 0; i < x.arr.Length; i++)
+            {
+                x.arr[i] = x.Check_xi(x.arr[i]);
+            };
+
+            for (int i = 0; i < z.arr.Length; i++)
+            {
+                z.arr[i] = z.Check_zi(x.arr[i], y.arr[i]);
+            }
+
+            Console.WriteLine("Масив Х:");
+            x.Getarrays(x.arr);
+            Console.WriteLine("Масив Y:");
+            y.Getarrays(y.arr);
+            Console.WriteLine("Масив Z:");
+            z.Getarrays(z.arr);
+
+            string path = @"D:\Унік\ООП\task5.txt";
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine("Масив Х:");
+                    foreach (int i in x.arr)
+                    {
+                        sw.Write($"[{i}];");
+                    }
+                    sw.WriteLine();
+                    sw.WriteLine("Масив Y:");
+                    foreach (int i in y.arr)
+                    {
+                        sw.Write($"[{i}];");
+                    }
+                    sw.WriteLine();
+                    sw.WriteLine("Масив Z:");
+                    foreach (int i in z.arr)
+                    {
+                        sw.Write($"[{i}];");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
